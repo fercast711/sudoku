68219d96e512e859a474b065396829e77800f411
fercast711 <fercast7.fc@gmail.com>
2024-09-02 16:56:39 -0600

    created function backtracking and added atribute byAI

--- backend/api/views.py
+++ backend/api/views.py
@@ -12,34 +12,52 @@ lvl = getattr(settings, 'LOG_LEVEL', logging.DEBUG)
 logging.basicConfig(format=fmt, level=lvl)
 model = load_model("api/model_save.keras")
 
+def solveSudokuModel(quiz):
+    quiz = (np.array(quiz).reshape(9,9,1)/9) - 0.5
+    while True:
+        predict = model.predict(quiz.reshape(1,9,9,1)).squeeze()
+        quizSolved = np.argmax(predict, axis=1).reshape((9, 9)) + 1
+        probSolved = np.around(np.max(predict, axis=1).reshape((9, 9)), 2)
+        
+        quiz = ((quiz+0.5)*9).reshape(9,9)
+        zeroMask = (quiz==0)
+
+        if zeroMask.sum() == 0:
+            break
+
+        probSolvedNew = probSolved * zeroMask
+        idx = np.argmax(probSolvedNew)
+        x, y = (idx // 9), (idx % 9)
+
+        quiz[x][y] = quizSolved[x][y]
+        quiz = (quiz / 9) - 0.5
+    
+    return quiz
+
+def solveSudokuBacktracking(quiz):
+    def solveSudoku():
+        pass
+    pass
+
 class Sudoku(APIView):
     def post(self, request):
         quiz = request.data.get('quiz', None)
+        byIA = request.data.get('byIA', None)
         if quiz is None:
             return Response({'error': 'No se encontró la clave "quiz" en los datos de la solicitud'}, status=status.HTTP_400_BAD_REQUEST)
         
-        quiz = list(map(int, list(quiz)))
-        quiz = (np.array(quiz).reshape(9,9,1)/9) - 0.5
-
-        while True:
-            predict = model.predict(quiz.reshape(1,9,9,1)).squeeze()
-            quizSolved = np.argmax(predict, axis=1).reshape((9, 9)) + 1
-            probSolved = np.around(np.max(predict, axis=1).reshape((9, 9)), 2)
-            
-            quiz = ((quiz+0.5)*9).reshape(9,9)
-            zeroMask = (quiz==0)
-
-            if zeroMask.sum() == 0:
-                break
-
-            probSolvedNew = probSolved * zeroMask
-            idx = np.argmax(probSolvedNew)
-            x, y = (idx // 9), (idx % 9)
-
-            quiz[x][y] = quizSolved[x][y]
-            quiz = (quiz / 9) - 0.5
+        if byIA is None:
+            return Response({'error': 'No se encontró la clave "byIA" en los datos de la solicitud'}, status=status.HTTP_400_BAD_REQUEST)
         
+        quiz = list(map(int, list(quiz)))
+        solution =  None
 
-        #quiz = quiz.reshape(81)
-        res = {'solution': quiz}
+        if byIA: 
+            solution = solveSudokuModel(quiz)
+        else: 
+            quiz = np.array(quiz).reshape((9,9))
+            solution = solveSudokuBacktracking(quiz)
+        
+        logging.debug(quiz)
+        res = {'solution': solution}
         return Response(res, status=status.HTTP_200_OK)
