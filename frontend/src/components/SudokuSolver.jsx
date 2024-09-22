3f5ec0b2dce9b2edcc9bad8de7eabae4cd83256a
JRafaelLobo <jose.lobo@unitec.edu>
2024-09-21 23:06:16 -0600

    Al fin sudoku funcional

--- frontend/src/components/SudokuSolver.jsx
+++ frontend/src/components/SudokuSolver.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import '../styles/SudokuSolver.css';
 
@@ -8,21 +8,124 @@ const SudokuSolver = () => {
   const [selectedCell, setSelectedCell] = useState(null);
   const [gridValues, setGridValues] = useState(Array(81).fill(''));
   const [solution, setSolution] = useState(null);
-  const [error, setError] = useState('');
+  const [error, setError] = useState(0);
 
+  const [SolutionGrid, setSolutionGrid] = useState(Array(81).fill(0));
+
+  /**
+   * Se Ejecuta al ingresar
+   */
+  const generateSolutionGrid = useEffect(() => {
+
+    /**
+     * Al generar la funcion verifica que estas este dentro de las reglas de sudoku
+     */
+    function isSafe(grid, index, num) {
+      let row = Math.floor(index / 9);
+      let col = index % 9;
+
+      for (let i = 0; i < 9; i++) {
+        if (grid[row * 9 + i] === num || grid[i * 9 + col] === num) {
+          return false;
+        }
+      }
+
+      let startRow = row - row % 3;
+      let startCol = col - col % 3;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (grid[(startRow + i) * 9 + (startCol + j)] === num) {
+            return false;
+          }
+        }
+      }
+      return true;
+    }
+
+    /**
+     * rellena con numeros aleatorio la funcion
+     * @param {*} grid 
+     */
+    function fillGrid(grid) {
+      for (let i = 0; i < 81; i++) {
+        let num = Math.floor(Math.random() * 9) + 1;
+        let attempts = 0;
+
+        while (!isSafe(grid, i, num) && attempts < 50) {
+          num = Math.floor(Math.random() * 9) + 1;
+          attempts++;
+        }
+
+        if (attempts < 1000) {
+          grid[i] = num;
+        }
+      }
+    }
+
+    /**
+     * Elimina elementos dependiendo de la dificultad para poder iniciar el juego
+     * @param {Int8Array} grid 
+     * @param {int} difficulty 
+     */
+    function removeElements(grid, difficulty) {
+      let remainingCells = difficulty; // Cuantos números dejar visibles
+      while (remainingCells > 0) {
+        let index = Math.floor(Math.random() * 81);
+
+        if (grid[index] !== 0) {
+          grid[index] = 0;
+          remainingCells--;
+        }
+      }
+    };
+    const run = () => {
+      let tempGridSolution = Array(81);
+      fillGrid(tempGridSolution);
+      console.log(tempGridSolution);
+      setSolutionGrid(tempGridSolution);
+      let tempGridGame = [...tempGridSolution];
+      removeElements(tempGridGame, 20);
+      const convertedGrid = tempGridGame.map(num => num !== 0 ? num.toString() : '');
+      setGridValues(convertedGrid);
+    };
+    run();
+
+
+  }, []);
+
+
+  /**
+   * Se ejecuta cada vez que hacen click
+   * @param {*} row 
+   * @param {*} col 
+   */
   const handleCellClick = (row, col) => {
     setSelectedCell({ row, col });
   };
 
+  /**
+   * Se ejecuta cada vez que se detecta un cambio en las celdas
+   * @param {*} row 
+   * @param {*} col 
+   * @param {*} event 
+   */
   const handleCellChange = (row, col, event) => {
     const value = event.target.value;
     if (/^[1-9]$/.test(value) || value === '') { // Permitir solo números del 1 al 9
       const newGridValues = [...gridValues];
       newGridValues[row * 9 + col] = value;
       setGridValues(newGridValues);
+      console.log(SolutionGrid);
+      //SolutionGrid[row * 9 + col].toString()
+      if (newGridValues[row * 9 + col] !== SolutionGrid[row * 9 + col].toString() && value !== '') {
+        setError(error + 1);
+      }
     }
   };
 
+  /**
+   * Se conecta con el backend para conseguir una solucion
+   */
   const solveSudoku = async () => {
     try {
       const response = await fetch('http://localhost:8000/api/sudoku/', {
@@ -30,7 +133,7 @@ const SudokuSolver = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-         // Enviar el grid
+        // Enviar el grid
         body: JSON.stringify({ quiz: gridValues.join('') }),
       });
 
@@ -38,7 +141,7 @@ const SudokuSolver = () => {
         const data = await response.json();
         if (data.solution) {
           // Actualizar el grid con la solución
-          setSolution(data.solution.flat()); 
+          setSolution(data.solution.flat());
         } else {
           setError('No se pudo resolver el Sudoku');
         }
@@ -51,6 +154,10 @@ const SudokuSolver = () => {
     }
   };
 
+  /**
+   * Es el artefacto principa
+   * @returns Interfaz
+   */
   const renderGrid = () => {
     return (solution || gridValues).map((value, index) => {
       const row = Math.floor(index / 9);
@@ -69,7 +176,7 @@ const SudokuSolver = () => {
           maxLength="1"
 
           // Deshabilitar inputs si ya hay una solución
-          disabled={!!solution} 
+          disabled={!!solution}
         />
       );
     });
